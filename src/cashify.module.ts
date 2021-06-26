d118d6fe20f8e3655bc85ac9e0ee9a65e6eb9e82
Vahid Najafi <vahid.vdn@gmail.com>
2021-06-26 12:30:43 +0430

    docs(module): add some comments to clarify about providers

--- src/cashify.module.ts
+++ src/cashify.module.ts
@@ -4,7 +4,19 @@ import { CASHIFY, CashifyService, CASHIFY_OPTIONS } from './';
 
 @Module({})
 export class CashifyModule {
+
+  /**
+   * @param options a simple configuration object
+   * @returns DynamicModule object
+   */
   public static forRoot(options): DynamicModule {
+
+    /**
+    * To have access to the Cashify's instance by CASHIFY token injection
+    * The reason we export it too, is that we can have direct access in consumer module,
+    * so if there is a new method added in future for core module by it's author,
+    * consumer module can have access to it
+     */
     const CashifyProvider = {
       provide: CASHIFY,
       useValue: new Cashify(options),
@@ -18,12 +30,29 @@ export class CashifyModule {
       ]
     }
   }
+  
+  /**
+   * @param optionsAsync this is not a simple object of main options
+   * this is an async configuration with useFactory passed from consumer module
+   * @returns DynamicModule object
+   */
   public static forRootAsync(optionsAsync): DynamicModule {
+
+    /**
+    * This is going to be a factory provider and import in the list of providers
+    * This provider make the options value available. Since it's a provider,
+    * it can be injected in CashifyProvider
+     */
     const CashifyOptionProvider = {
       provide: CASHIFY_OPTIONS,
       useFactory: optionsAsync.useFactory,
       inject: optionsAsync.inject || []
     };
+
+    /**
+    * This is our main provider that is accessible within the service
+    * The same one in forRoot method (but resolve the options in an async way)
+     */
     const CashifyProvider = {
       provide: CASHIFY,
       useFactory: (options) => new Cashify(options),
