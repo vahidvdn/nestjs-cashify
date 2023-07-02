2c90298afe86f5f01c197a2aa7f18fa0510a7e15
Vahid Najafi <v.najafi@casenio.de>
2023-07-02 18:07:05 +0200

    docs: fix title of website

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Documentation of ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
