d2def38bc53fc4ff3ca3668c49648329813f9899
Vahid Najafi <v.najafi@casenio.de>
2023-07-02 18:08:32 +0200

    docs: correct edit url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/vahidvdn/nestjs-cashify/tree/master/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
